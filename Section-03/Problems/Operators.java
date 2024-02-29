86eaecd83e12396961233d4bd57bc4451c2e49da
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-02-29 21:14:44 +0530

    Adding new problem's Solution

--- Section-03/Problems/Operators.java
+++ Section-03/Problems/Operators.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Operators {
-    public static void main(String[] args) {
-        printTable(29);
-        // System.out.println("Sum of all odd numbers is : " + addOddSum(5));
-        // System.out.println("Factorial is :" + factorial());
-    }
+    // public static void main(String[] args) {
+    // // printTable(29);
+    // // System.out.println("Sum of all odd numbers is : " + addOddSum(5));
+    // // System.out.println("Factorial is :" + factorial());
+    // }
 
     /* Print multiplication table for a given number */
 
-    public static void printTable(int num) {
-        System.out.println("Table starts from here!!!");
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(num * i);
-            i++;
-        }
-    }
+    // public static void printTable(int num) {
+    // System.out.println("Table starts from here!!!");
+    // int i = 1;
+    // while (i <= 10) {
+    // System.out.println(num * i);
+    // i++;
+    // }
+    // }
 
     /* To sum all odd numbers from 1 to N. */
 
@@ -47,5 +47,28 @@ public class Operators {
     // input.close();
     // return factorial;
     // }
+
+    /* Sum of the input number's digits */
+
+    public static void main(String[] args) {
+        sumDigits();
+    }
+
+    public static void sumDigits() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int number = input.nextInt();
+
+        int sum = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            sum += digit;
+            number = number / 10;
+        }
+
+        System.out.println("Sum of the input's digit is : " + sum);
+
+        input.close();
+    }
 
 }
