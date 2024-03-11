6a4b170accc8be258fb77d6caacd3d9907fd40c9
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-03-12 02:13:10 +0530

    Adding new files

---
+++ Section-03/Problems/Array/maximumAndMinimum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class maximumAndMinimum {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int main(){
+        System.out.println("Enter the number : ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+        input.close();
+        return number;
+    }
+}
